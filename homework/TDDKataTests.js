b13fb479a572bd79da0b30d20ef423b05978aa9a
michael <v.b.n@mail.ru>
2016-11-01 13:16:05 +0300

    KATA one number

--- homework/TDDKataTests.js
+++ homework/TDDKataTests.js
@@ -1,10 +1,33 @@
 import assert from 'assert'
 
 
-function add(){
-    return 0
+
+function add(value){
+    if (value == ""){
+        return 0
+    }
+    return value
 }
 
 suite('String Calculator testing', function () {
+    suite('without init number',function(){
+        test('Empty value will return 0', function() {
+            let input = '';
+
+            let sum = add(input);
+
+            assert.equal(0, sum);
+        });
+    })
+
+    suite('one init number',function(){
+        test('1 value will return 1', function() {
+            let input = 1;
+
+            let sum = add(input);
+
+            assert.equal(1, sum);
+        });
+    })
 
 });
