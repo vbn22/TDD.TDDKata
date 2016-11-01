032721d68ab610b1e625fe5e3a8c2daf8f3e2aca
michael <v.b.n@mail.ru>
2016-11-01 13:55:51 +0300

    Kata2 several numbers

--- homework/TDDKataTests.js
+++ homework/TDDKataTests.js
@@ -1,10 +1,34 @@
 import assert from 'assert'
 
+function hasDelimiters(){
+    let result = 0;
+    for (let value of str.split(',')){
+        result += parseInt(value);
+    }
+    return result;
+}
 
 function add(str){
-    return 0
+    if (!str){
+        return 0
+    }
+    return hasDelimiters(str);
 }
 
 suite('String Calculator testing', function () {
+    suite('when input is empty', function () {
+        test('for empty as input string it will return 0', function() {
+            let input = '';
+            let sum = add(input);
+            assert.equal(0, sum);
+        });
+    });
 
+    suite('when input is several numbers', function () {
+        test('for 1,2,3 as input string it will return 6', function() {
+            let input = '1,2,3';
+            let sum = add(input);
+            assert.equal(6, sum);
+        });
+    });
 });
