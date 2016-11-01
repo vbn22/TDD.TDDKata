9c006b1828856404c362e4f752c8bf47db3cc53c
michael <v.b.n@mail.ru>
2016-11-01 14:30:33 +0300

    Kata2 custom delimiters

--- homework/TDDKataTests.js
+++ homework/TDDKataTests.js
@@ -1,8 +1,19 @@
 import assert from 'assert'
 
+
+function getCustomDelimiters(str){
+    return str.match(/\/\/(.*)\n/);
+}
+
 function hasDelimiters(str){
     let result = 0;
-    for (let value of str.split(/,|\n/)){
+    let delimiters = /,|\n/;
+    if (str.indexOf('//') > -1){
+        delimiters = getCustomDelimiters(str);
+        str = str.replace(delimiters[0],'');
+        delimiters = delimiters[1];
+    }
+    for (let value of str.split(delimiters)){
         result += parseInt(value);
     }
     return result;
@@ -32,11 +43,11 @@ suite('String Calculator testing', function () {
         });
     });
 
-    suite('when input string has several delimiters', function () {
-        test('for 1\\n2,3 as input string it will return 6', function() {
-            let input = '1\n2,3';
+    suite('when delimiter is custom char (//[delimiter]\\n[numbers…])', function () {
+        test('for //;\\n1;2;3 as input string it will return 6', function() {
+            let input = '//;\n1;2';
             let sum = add(input);
-            assert.equal(6, sum);
+            assert.equal(3, sum);
         });
     });
 
