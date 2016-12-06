eb79670b33501760c6d5665edf5e2661bd96cd74
michael <v.b.n@mail.ru>
2016-12-06 12:54:33 +0300

    1,2 is 3

--- homework/TDDKataTests.js
+++ homework/TDDKataTests.js
@@ -3,8 +3,14 @@ import assert from 'assert'
 function add(str){
     if (!str){
         return 0
+    } else {
+        let result = 0
+        for (let i of str.split(',')){
+            result += parseInt(i);
+        }
+        return result
     }
-    return 1
+
 }
 
 suite('String Calculator testing', function () {
@@ -13,6 +19,14 @@ suite('String Calculator testing', function () {
             let input = '';
             let sum = add(input);
             assert.equal(0, sum);
+        });
+    });
+
+    suite('when input is numbers', function () {
+        test('for 1,2 as input string it will return 3', function() {
+            let input = '1,2';
+            let sum = add(input);
+            assert.equal(3, sum);
         });
     });
 });
