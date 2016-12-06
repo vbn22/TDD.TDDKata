15d531dc7847ec444759bddf8a01a021f38278e5
michael <v.b.n@mail.ru>
2016-12-06 12:49:58 +0300

    empty string

--- homework/TDDKataTests.js
+++ homework/TDDKataTests.js
@@ -1,7 +1,18 @@
 import assert from 'assert'
 
-
+function add(str){
+    if (!str){
+        return 0
+    }
+    return 1
+}
 
 suite('String Calculator testing', function () {
-
+    suite('when input is empty', function () {
+        test('for empty as input string it will return 0', function() {
+            let input = '';
+            let sum = add(input);
+            assert.equal(0, sum);
+        });
+    });
 });
