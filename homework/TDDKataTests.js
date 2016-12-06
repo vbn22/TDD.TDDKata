3ce3c990d6d14982cf110e99a4b0e6dd4e5ac360
michael <v.b.n@mail.ru>
2016-12-06 13:01:50 +0300

    1\n2,3 is 6

--- homework/TDDKataTests.js
+++ homework/TDDKataTests.js
@@ -1,14 +1,24 @@
 import assert from 'assert'
 
+
+function get_numbers(str){
+    let delimiters = /,|\n/;
+    return str.split(delimiters);
+}
+
+function add_numbers(str){
+    let result = 0;
+    for (let i of get_numbers(str)){
+        result += parseInt(i);
+    }
+    return result
+}
+
 function add(str){
     if (!str){
-        return 0
+        return 0;
     } else {
-        let result = 0
-        for (let i of str.split(',')){
-            result += parseInt(i);
-        }
-        return result
+        return add_numbers(str);
     }
 
 }
@@ -33,6 +43,14 @@ suite('String Calculator testing', function () {
     suite('when input is several numbers', function () {
         test('for 1,2,3 as input string it will return 6', function() {
             let input = '1,2,3';
+            let sum = add(input);
+            assert.equal(6, sum);
+        });
+    });
+
+    suite('when input contain several delimiters', function () {
+        test('for 1\n2,3 as input string it will return 6', function() {
+            let input = '1\n2,3';
             let sum = add(input);
             assert.equal(6, sum);
         });
